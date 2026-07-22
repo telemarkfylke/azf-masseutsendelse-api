ed6499b560cadd1597ff4357a3ef48b4fbcde9fe
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-07-22 13:46:26 +0200

    fix: Added possibility to use a development user for testing against web locally

--- src/sharedcode/auth/auth.js
+++ src/sharedcode/auth/auth.js
@@ -1,25 +1,37 @@
+const { logger } = require("@vestfoldfylke/loglady");
 const azureAd = require("./lib/azuread");
-/*
-  Auth function
-*/
+
+const testUser = {
+  name: "timetrigger",
+  id: "timetrigger",
+  department: "timetrigger",
+  email: "timetrigger@telemarkfylke.no"
+};
+
 /**
  * Auth's the request
  * @param {object} req Azure function request
  * @returns
  */
 async function auth(req) {
+  if (process.env.NODE_ENV?.toLowerCase() === "test") {
+    // Return a default timetrigger user in test mode since no auth header is provided
+    return testUser;
+  }
+
   const authValue = req.headers.get("authorization") || req.headers.get("Authorization");
   if (!authValue) {
-    if (process.env.NODE_ENV?.toLowerCase() !== "test") {
+    if (process.env.NODE_ENV?.toLowerCase() !== "development") {
       throw new Error("No authorization header was provided");
     }
 
-    // Return a default timetrigger user in test mode since no auth header is provided
+    // Return a dev user in development mode since no auth header is provided - for testing against web
+    logger.warn("Using a development user based on AUTH_DEVELOPMENT_* env variables");
     return {
-      name: "timetrigger",
-      id: "timetrigger",
-      department: "timetrigger",
-      email: "timetrigger@telemarkfylke.no"
+      name: process.env.AUTH_DEVELOPMENT_NAME ?? testUser.name,
+      id: process.env.AUTH_DEVELOPMENT_ID ?? testUser.id,
+      department: process.env.AUTH_DEVELOPMENT_DEPARTMENT ?? testUser.department,
+      email: process.env.AUTH_DEVELOPMENT_EMAIL ?? testUser.email
     };
   }
 
